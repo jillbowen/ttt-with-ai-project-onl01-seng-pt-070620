5e22c1fe0d2c61631bd00e5ce44717d20e6f3fec
Jill Short <jillian.b.short@gmail.com>
2020-08-15 01:05:28 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,23 +2,23 @@ module Players
   class Computer < Player 
     
     def move(board)
-      if board.cells[4] = " "
+      if board.cells[4] == " "
         puts "5"
-      elsif board.cells[0] = " "
+      elsif board.cells[0] == " "
         puts "1"
-      elsif board.cells[2] = " "
+      elsif board.cells[2] == " "
         puts "3"
-      elsif board.cells[6] = " "
+      elsif board.cells[6] == " "
         puts "7"
-      elsif board.cells[8] = " "
+      elsif board.cells[8] == " "
         puts "9"
-      elsif @board.cells[1] = " "
+      elsif @board.cells[1] == " "
         puts "2"
-      elsif @board.cells[3] = " "
+      elsif @board.cells[3] == " "
         puts "4"
-      elsif @board.cells[5] = " "
+      elsif @board.cells[5] == " "
         puts "6"
-      else @board.cells[7] = " "
+      else @board.cells[7] == " "
         puts "8"
       end
     end

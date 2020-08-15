66edcf215e63f7c4efb519e4d38ca51ad717045a
Jill Short <jillian.b.short@gmail.com>
2020-08-15 01:14:41 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,23 +3,23 @@ module Players
     
     def move(board)
       if board.cells[4] == " "
-        return "5"
+        "5"
       elsif board.cells[0] == " "
-        return "1"
+        "1"
       elsif board.cells[2] == " "
-        return "3"
+        "3"
       elsif board.cells[6] == " "
-        return "7"
+        "7"
       elsif board.cells[8] == " "
-        return "9"
+        "9"
       elsif board.cells[1] == " "
-        return "2"
+        "2"
       elsif board.cells[3] == " "
-        return "4"
+        "4"
       elsif board.cells[5] == " "
-        return "6"
+        "6"
       else board.cells[7] == " "
-        return "8"
+        "8"
       end
     end
     
